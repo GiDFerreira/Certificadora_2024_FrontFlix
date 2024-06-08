400fa88eade6b8c91a5926320bf7c4ef2a126c1c
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2024-06-07 23:11:11 -0300

    slides + projeto de currículo

--- Projetos/Curriculo/javascript.js
+++ Projetos/Curriculo/javascript.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const downloadButton = document.getElementById('downloadCV');
+document.addEventListener('DOMContentLoaded', function(){
 
-    downloadButton.addEventListener('click', function() {
+    const button = document.getElementById('downloadCV');
+    button.addEventListener('click', function(){
         const link = document.createElement('a');
         link.href = 'assets/curriculo.pdf';
         link.download = 'curriculo.pdf';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-    });
-});
+    })
+})
